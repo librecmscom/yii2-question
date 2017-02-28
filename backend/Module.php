0cdb345dd32b50f1a8fec67e68d0577b4f589272
xuchao <85825770@qq.com>
2017-02-28 10:48:33 +0800

    后台入口

---
+++ backend/Module.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\question\backend;
+
+class Module extends \yuncms\question\Module
+{
+
+}
