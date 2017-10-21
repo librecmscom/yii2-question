ef1ebf31c227edcd5f2fff1d3cf47b719f590e7e
Peter <xutongle@gmail.com>
2017-10-21 15:43:42 +0800

    update

--- migrations/M161111090444Create_question_table.php
+++ migrations/M161111090444Create_question_table.php
@@ -14,23 +14,23 @@ class M161111090444Create_question_table extends Migration
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
         $this->createTable('{{%question}}', [
-            'id' => $this->primaryKey()->unsigned(),
-            'user_id' => $this->integer()->unsigned()->notNull(),
-            'title' => $this->string(100)->notNull(),
-            'alias' => $this->string(100)->notNull(),
-            'price'=>$this->smallInteger(6)->unsigned()->defaultValue(0),
-            'hide'=>$this->boolean()->defaultValue(false),
-            'content' => $this->text()->notNull(),
-            'answers' => $this->integer()->unsigned()->notNull()->defaultValue(0),
-            'views' => $this->integer()->unsigned()->notNull()->defaultValue(0),
-            'followers' => $this->integer()->unsigned()->notNull()->defaultValue(0),
-            'collections' => $this->integer()->unsigned()->notNull()->defaultValue(0),
-            'comments'=>$this->integer()->unsigned()->notNull()->defaultValue(0),
-            'status' => $this->smallInteger()->unsigned()->notNull()->defaultValue(1),
-            'created_at' => $this->integer()->unsigned()->notNull(),
-            'updated_at' => $this->integer()->unsigned()->notNull(),
+            'id' => $this->primaryKey()->unsigned()->comment('Id'),
+            'user_id' => $this->integer()->unsigned()->notNull()->comment('User Id'),
+            'title' => $this->string(100)->notNull()->comment('Title'),
+            'alias' => $this->string(100)->notNull()->comment('Alias'),
+            'price' => $this->smallInteger(6)->unsigned()->defaultValue(0)->comment('Price Id'),
+            'hide' => $this->boolean()->defaultValue(false)->comment('Hide'),
+            'content' => $this->text()->notNull()->comment('Content'),
+            'answers' => $this->integer()->unsigned()->notNull()->defaultValue(0)->comment('Answers'),
+            'views' => $this->integer()->unsigned()->notNull()->defaultValue(0)->comment('Views'),
+            'followers' => $this->integer()->unsigned()->notNull()->defaultValue(0)->comment('Followers'),
+            'collections' => $this->integer()->unsigned()->notNull()->defaultValue(0)->comment('Collections'),
+            'comments' => $this->integer()->unsigned()->notNull()->defaultValue(0)->comment('Comments'),
+            'status' => $this->smallInteger()->unsigned()->notNull()->defaultValue(1)->comment('Status'),
+            'created_at' => $this->integer()->unsigned()->notNull()->comment('Created At'),
+            'updated_at' => $this->integer()->unsigned()->notNull()->comment('Updated At'),
         ], $tableOptions);
-        $this->addForeignKey('question_ibfk_1', '{{%question}}', 'user_id', '{{%user}}', 'id', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('{{%question_fk_1}}', '{{%question}}', 'user_id', '{{%user}}', 'id', 'CASCADE', 'CASCADE');
     }
 
     public function down()
